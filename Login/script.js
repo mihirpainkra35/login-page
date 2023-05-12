da146f60d129b1c322cf73d4f8747d009c4e873d
Mihir painkra <mihirpaikra35@gmail.com>
2023-05-12 13:52:07 +0530

    first commit

---
+++ Login/script.js
@@ -0,0 +1,7 @@
+function showLoginForm() {
+	document.getElementById("login-form").style.display = "block";
+}
+
+function hideLoginForm() {
+	document.getElementById("login-form").style.display = "none";
+}
